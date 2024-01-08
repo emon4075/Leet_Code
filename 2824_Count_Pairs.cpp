55a112ddcff2529cdc5dbec1980fe490b48a4956
emon4075 <mdabdullahemon4075@gmail.com>
2024-01-08 08:01:55 +0600

    Solved Problem On Binary Search

---
+++ 2824_Count_Pairs.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+class Solution
+{
+public:
+    int countPairs(vector<int> &nums, int target)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] < target && i < j)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
