a20f7c42aed9256a366b8ad924af0b0ccfc35e92
Md Abdullah Al Mamun Emon <87023992+emon4075@users.noreply.github.com>
2025-02-04 18:01:24 +0600

    Time: 15 ms (26.5%), Space: 31.5 MB (73.03%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,7 +4,6 @@ public:
         sort(nums.begin(), nums.end(), greater<>());
         int Result1 = nums[0] * nums[1] * nums[2];
         int Result2 = nums[nums.size() - 1] * nums[nums.size() - 2] * nums[0];
-        // int Result3 = nums[0] * nums[1] * nums[2];
         return max(Result1, Result2);
     }
 };
