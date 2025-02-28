3195db433c3b755cd748d628f6260740d2a29eed
Md Abdullah Al Mamun Emon <87023992+emon4075@users.noreply.github.com>
2025-02-28 16:06:11 +0600

    Time: 0 ms (100%), Space: 10.2 MB (7.51%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,74 @@
+class Stack {
+    int size;
+    char* array;
+    int top;
+
+public:
+    Stack(int capacity = 1e5) {
+        top = -1;
+        size = capacity;
+        array = new char[size];
+    }
+    ~Stack() {
+        delete[] array;
+        // cout << "Stack memory freed!" << endl;
+    }
+    void push(char x) {
+        if (top + 1 == size) {
+            // cout << "Stack overflow!" << endl;
+            return;
+        }
+        top++;
+        array[top] = x;
+    }
+    char pop() {
+        if (isEmpty()) {
+            // cout << "Stack underflow!" << endl;
+            return '\0';
+        }
+        char result = array[top];
+        top--;
+        return result;
+    }
+    char getTop() {
+        if (isEmpty()) {
+            // cout << "Stack is empty!" << endl;
+            return '\0';
+        }
+        return array[top];
+    }
+    int getSize() { return top + 1; }
+    bool isEmpty() { return top == -1; }
+};
+
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        Stack ST;
+        bool Track[26];
+        int Last[26];
+        memset(Track, false, sizeof(Track));
+        for (int i = 0; i < s.size(); i++) {
+            Last[s[i] - 'a'] = i;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            int IDX = s[i] - 'a';
+            if (Track[IDX]) {
+                continue;
+            }
+            while (!ST.isEmpty() && s[i] < ST.getTop() &&
+                   Last[ST.getTop() - 'a'] > i) {
+                Track[ST.getTop() - 'a'] = false;
+                ST.pop();
+            }
+            ST.push(s[i]);
+            Track[IDX] = true;
+        }
+        string Result;
+        while (!ST.isEmpty()) {
+            Result.push_back(ST.pop());
+        }
+        reverse(Result.begin(), Result.end());
+        return Result;
+    }
+};
